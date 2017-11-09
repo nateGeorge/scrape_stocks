ba00e0d4aaa00fdf08801f4a0f9511d52679e155
nateGeorge <wordsforthewise@gmail.com>
2017-11-08 21:10:18 -0700

    FEAT: add utils for getting home dir

---
+++ utils.py
@@ -0,0 +1,12 @@
+import os
+
+def get_home_dir(repo_name='scrape_stocks'):
+    cwd = os.getcwd()
+    cwd_list = cwd.split('/')
+    repo_position = [i for i, s in enumerate(cwd_list) if s == repo_name]
+    if len(repo_position) > 1:
+        print("error!  more than one intance of repo name in path")
+        return None
+
+    home_dir = '/'.join(cwd_list[:repo_position[0] + 1]) + '/'
+    return home_dir
