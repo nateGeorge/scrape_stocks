32150b62dec8fef34c5bf348ef3bafaf50a7e24d
nateGeorge <wordsforthewise@gmail.com>
2017-11-12 16:07:27 -0700

    FEAT: remove junk from short squeeze data, and use multiprocessing

--- short_squeeze_eda.py
+++ short_squeeze_eda.py
@@ -6,15 +6,54 @@ import calendar
 # installed
 import pandas as pd
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
 # custom
 from utils import get_home_dir
 
 HOME_DIR = get_home_dir(repo_name='scrape_stocks')
 
-def load_all_short_squeeze_data():
+def stuff(f, dates_df, rev_cal_dict):
+    print(f)
+    df = pd.read_excel(f)
+    # fixes truecar ticker error; is listed as '1' in the data
+    tc_idx = df[df['ShortSqueeze.com™ Short Interest Data'] == 'Truecar Incorporated'].index[0]
+    df.at[tc_idx, 'Symbol'] = 'TRUE'
+    # df.set_value(tc_idx, 'Symbol', 'TRUE')  # old way of doing it
+    # cuts off crap at the end -- old way of doing it was too verbose, so commentetd out
+    # end_idxs = df.index[df.iloc[:, 0].str.contains('ShortSqueeze.com: Master Spreadsheet', case=False).fillna(False) | df.iloc[:, 0].str.contains('ShortSqueeze.comï¿½: Master Spreadsheetï¿½ ', case=False).fillna(False)]
+    end_idxs = df.index[df.iloc[:, 0].str.contains('Master Spreadsheet', case=False).fillna(False)]
+    if len(end_idxs) > 1:
+        print('WARNING: matched more than 1 end index')
+    elif len(end_idxs) < 1:
+        print('WARNING: no end index found for:', f)
+
+    # drop the end junk, and the fully missing rows (usually 2 at the end)
+    df = df.iloc[:end_idxs[0]]
+    df.drop(df.index[df.isnull().all(1)], inplace=True)
+    # get the date from the dates_df
+    date = f.split('/')[-1][9:16]
+    year = date[:4]
+    month_num = int(date[4:6])
+    month = rev_cal_dict[month_num]
+    ab = date[-1].upper()
+    t_df = dates_df[year]
+    date = '-'.join([year,
+                    str(month_num).zfill(2),
+                    str(t_df[t_df[int(year)] == (month + ' ' + ab)]['NASDAQ®'].values[0]).zfill(2)])
+    date = pd.to_datetime(date, format='%Y-%m-%d')
+    df['Date'] = date
+
+    return df
+
+
+def load_all_short_squeeze_data(load_old=True):
+    """
+    loads all historical data
+    :param load_old: boolean, if true, will load an existing h5 file
+    """
     filename = HOME_DIR + 'short_squeeze_data.h5'
-    if os.path.exists(filename):
+    if os.path.exists(filename) and load_old:
         return pd.read_hdf(filename)
 
     cal_dict = {v: k for k, v in enumerate(calendar.month_name)}
@@ -25,23 +64,20 @@ def load_all_short_squeeze_data():
 
     dfs = []
     files = glob.glob(HOME_DIR + 'data/short_squeeze.com/*.xlsx')
-    for f in tqdm(files):
-        df = pd.read_excel(f)
-        tc_idx = df[df['ShortSqueeze.com™ Short Interest Data'] == 'Truecar Incorporated'].index[0]
-        df.at[tc_idx, 'Symbol'] = 'TRUE'
-        # df.set_value(tc_idx, 'Symbol', 'TRUE')  # old way of doing it
-        date = f.split('/')[-1][9:16]
-        year = date[:4]
-        month_num = int(date[4:6])
-        month = rev_cal_dict[month_num]
-        ab = date[-1].upper()
-        t_df = dates_df[year]
-        date = '-'.join([year,
-                        str(month_num).zfill(2),
-                        str(t_df[t_df[int(year)] == (month + ' ' + ab)]['NASDAQ®'].values[0]).zfill(2)])
-        date = pd.to_datetime(date, format='%Y-%m-%d')
-        df['Date'] = date
-        dfs.append(df)
+
+    dfs = []
+    jobs = []
+    with ProcessPoolExecutor(max_workers=None) as executor:
+        for f in files:
+            r = executor.submit(stuff,
+                                f,
+                                dates_df,
+                                rev_cal_dict)
+            jobs.append((f, r))
+
+    for f, r in jobs:
+        if r.result() is not None:
+            dfs.append(r.result())
 
     full_df = pd.concat(dfs)
     drop_cols = ['Record Date',
