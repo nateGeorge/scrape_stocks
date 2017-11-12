79388d19d1cb0bdced5abc9e4f8867754597a592
nateGeorge <wordsforthewise@gmail.com>
2017-11-12 14:39:39 -0700

    FEAT: explore short data

--- short_squeeze_eda.py
+++ short_squeeze_eda.py
@@ -13,7 +13,7 @@ from utils import get_home_dir
 HOME_DIR = get_home_dir(repo_name='scrape_stocks')
 
 def load_all_short_squeeze_data():
-    filename = 'short_squeeze_data.h5'
+    filename = HOME_DIR + 'short_squeeze_data.h5'
     if os.path.exists(filename):
         return pd.read_hdf(filename)
 
@@ -21,12 +21,15 @@ def load_all_short_squeeze_data():
     del cal_dict['']
     rev_cal_dict = {v: k for k, v in cal_dict.items()}
 
-    dates_df = pd.read_excel('short_squeeze_release_dates.xlsx', None)
+    dates_df = pd.read_excel(HOME_DIR + 'short_squeeze_release_dates.xlsx', None)
 
     dfs = []
     files = glob.glob(HOME_DIR + 'data/short_squeeze.com/*.xlsx')
     for f in tqdm(files):
         df = pd.read_excel(f)
+        tc_idx = df[df['ShortSqueeze.com™ Short Interest Data'] == 'Truecar Incorporated'].index[0]
+        df.at[tc_idx, 'Symbol'] = 'TRUE'
+        # df.set_value(tc_idx, 'Symbol', 'TRUE')  # old way of doing it
         date = f.split('/')[-1][9:16]
         year = date[:4]
         month_num = int(date[4:6])
@@ -37,7 +40,7 @@ def load_all_short_squeeze_data():
                         str(month_num).zfill(2),
                         str(t_df[t_df[int(year)] == (month + ' ' + ab)]['NASDAQ®'].values[0]).zfill(2)])
         date = pd.to_datetime(date, format='%Y-%m-%d')
-        df['date'] = date
+        df['Date'] = date
         dfs.append(df)
 
     full_df = pd.concat(dfs)
@@ -56,11 +59,12 @@ def load_all_short_squeeze_data():
     return full_df
 
 
-def get_short_interest_cols(full_df=None):
+def get_short_interest_data(full_df=None):
     if full_df is None:
         full_df = load_all_short_squeeze_data()
+
     cols = ['Symbol',
-            'date',
+            'Date',
             'Total Short Interest',
             'Days to Cover',
             'Short % of Float',
@@ -69,4 +73,12 @@ def get_short_interest_cols(full_df=None):
             'Shares: Float',
             'Short Squeeze Ranking™']
 
-    return full_df[cols]
+    return full_df[cols].rename(columns={'Short Squeeze Ranking™': 'Short Squeeze Ranking'})
+
+
+def get_stocks():
+    """
+    returns stocks with data from shortsqueeze.com
+    """
+    full_df = load_all_short_squeeze_data()
+    return full_df['Symbol'].unique()
