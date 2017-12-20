dcf089168cee4016cafe931571b5b75300e7a280
nateGeorge <wordsforthewise@gmail.com>
2017-12-19 22:01:47 -0700

    add some error logs

---
+++ huge_error_wtf.py
@@ -0,0 +1,324 @@
+ZYNE
+ERROR:root:Internal Python error in the inspect module.
+Below is the traceback from this internal error.
+
+concurrent.futures.process._RemoteTraceback:
+"""
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 141, in _new
+_conn
+    (self.host, self.port), self.timeout, **extra_kw)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/connection.py", line 83, in
+create_connection
+    raise err
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/connection.py", line 73, in
+create_connection
+    sock.connect(sa)
+OSError: [Errno 101] Network is unreachable
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 601, in
+urlopen
+    chunked=chunked)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 346, in
+_make_request
+    self._validate_conn(conn)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 850, in
+_validate_conn
+    conn.connect()
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 284, in conn
+ect
+    conn = self._new_conn()
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 150, in _new
+_conn
+    self, "Failed to establish a new connection: %s" % e)
+urllib3.exceptions.NewConnectionError: <urllib3.connection.VerifiedHTTPSConnection objec
+t at 0x7f3411777c50>: Failed to establish a new connection: [Errno 101] Network is unrea
+chable
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/requests/adapters.py", line 440, in send
+    timeout=timeout
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 639, in
+urlopen
+    _stacktrace=sys.exc_info()[2])
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/retry.py", line 388, in incr
+ement
+    raise MaxRetryError(_pool, url, error or ResponseError(cause))
+urllib3.exceptions.MaxRetryError: HTTPSConnectionPool(host='query2.finance.yahoo.com', p
+ort=443): Max retries exceeded with url: /v11/finance/quoteSummary/CCO?modules=defaultKe
+yStatistics,assetProfile,financialData,calendarEvents,incomeStatementHistory,cashflowSta
+tementHistory,balanceSheetHistory (Caused by NewConnectionError('<urllib3.connection.Ver
+ifiedHTTPSConnection object at 0x7f3411777c50>: Failed to establish a new connection: [E
+rrno 101] Network is unreachable',))
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/lib/python3.5/concurrent/futures/process.py", line 175, in _process_worker
+    r = call_item.fn(*call_item.args, **call_item.kwargs)
+  File "/home/nate/github/scrape_stocks/scrape_stockdata.py", line 569, in scrape_a_tick
+er_mongo
+    res = req.get(url, timeout=10)
+  File "/usr/local/lib/python3.5/dist-packages/requests/api.py", line 72, in get
+    return request('get', url, params=params, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/api.py", line 58, in request
+    return session.request(method=method, url=url, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/sessions.py", line 508, in reque
+st
+    resp = self.send(prep, **send_kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/sessions.py", line 618, in send
+    r = adapter.send(request, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/adapters.py", line 508, in send
+    raise ConnectionError(e, request=request)
+requests.exceptions.ConnectionError: HTTPSConnectionPool(host='query2.finance.yahoo.com'
+, port=443): Max retries exceeded with url: /v11/finance/quoteSummary/CCO?modules=defaul
+tKeyStatistics,assetProfile,financialData,calendarEvents,incomeStatementHistory,cashflow
+StatementHistory,balanceSheetHistory (Caused by NewConnectionError('<urllib3.connection.
+VerifiedHTTPSConnection object at 0x7f3411777c50>: Failed to establish a new connection:
+ [Errno 101] Network is unreachable',))
+"""
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py", line $
+910, in run_code
+    exec(code_obj, self.user_global_ns, self.user_ns)
+  File "<ipython-input-2-2c9937c17e38>", line 1, in <module>
+daily_scrape_data()                                                       [178/1898]
+File "/home/nate/github/scrape_stocks/scrape_stockdata.py", line 638, in daily_scrape_
+data
+print('scraping...')
+File "/home/nate/github/scrape_stocks/scrape_stockdata.py", line 549, in scrape_all_ti
+ckers_mongo_parallel
+if r.result() is not None:
+File "/usr/lib/python3.5/concurrent/futures/_base.py", line 398, in result
+return self.__get_result()
+File "/usr/lib/python3.5/concurrent/futures/_base.py", line 357, in __get_result
+raise self._exception
+requests.exceptions.ConnectionError: None: Max retries exceeded with url: /v11/finance/q
+uoteSummary/CCO?modules=defaultKeyStatistics,assetProfile,financialData,calendarEvents,i
+ncomeStatementHistory,cashflowStatementHistory,balanceSheetHistory (Caused by None)
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+File "/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py", line 1
+828, in showtraceback
+stb = value._render_traceback_()
+AttributeError: 'ConnectionError' object has no attribute '_render_traceback_'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+File "/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py", line 1090, in g
+et_records
+return _fixed_getinnerframes(etb, number_of_lines_of_context, tb_offset)
+File "/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py", line 311, in wr
+apped
+return f(*args, **kwargs)
+File "/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py", line 345, in _f
+ixed_getinnerframes
+records = fix_frame_records_filenames(inspect.getinnerframes(etb, context))
+File "/usr/lib/python3.5/inspect.py", line 1453, in getinnerframes
+frameinfo = (tb.tb_frame,) + getframeinfo(tb, context)
+File "/usr/lib/python3.5/inspect.py", line 1410, in getframeinfo
+filename = getsourcefile(frame) or getfile(frame)
+File "/usr/lib/python3.5/inspect.py", line 672, in getsourcefile
+if getattr(getmodule(object, filename), '__loader__', None) is not None:
+File "/usr/lib/python3.5/inspect.py", line 718, in getmodule
+os.path.realpath(f)] = module.__name__
+AttributeError: module has no attribute '__name__'
+---------------------------------------------------------------------------
+_RemoteTraceback                          Traceback (most recent call last)
+_RemoteTraceback:
+
+Traceback (most recent call last):                                            [130/1898]
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 141, in _new
+_conn
+    (self.host, self.port), self.timeout, **extra_kw)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/connection.py", line 83, in
+create_connection
+    raise err
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/connection.py", line 73, in
+create_connection
+    sock.connect(sa)
+OSError: [Errno 101] Network is unreachable
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 601, in
+urlopen
+    chunked=chunked)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 346, in
+_make_request
+    self._validate_conn(conn)
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 850, in
+_validate_conn
+    conn.connect()
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 284, in conn
+ect
+    conn = self._new_conn()
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connection.py", line 150, in _new
+_conn
+    self, "Failed to establish a new connection: %s" % e)
+urllib3.exceptions.NewConnectionError: <urllib3.connection.VerifiedHTTPSConnection objec
+t at 0x7f3411777c50>: Failed to establish a new connection: [Errno 101] Network is unrea
+chable
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.5/dist-packages/requests/adapters.py", line 440, in send
+    timeout=timeout
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 639, in
+urlopen
+    _stacktrace=sys.exc_info()[2])
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/retry.py", line 388, in incr
+ement
+    raise MaxRetryError(_pool, url, error or ResponseError(cause))
+urllib3.exceptions.MaxRetryError: HTTPSConnectionPool(host='query2.finance.yahoo.com', p
+ort=443): Max retries exceeded with url: /v11/finance/quoteSummary/CCO?modules=defaultKe
+yStatistics,assetProfile,financialData,calendarEvents,incomeStatementHistory,cashflowSta
+tementHistory,balanceSheetHistory (Caused by NewConnectionError('<urllib3.connection.Ver
+ifiedHTTPSConnection object at 0x7f3411777c50>: Failed to establish a new connection: [E
+rrno 101] Network is unreachable',))
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):                                             [94/1898]
+  File "/usr/local/lib/python3.5/dist-packages/requests/adapters.py", line 440, in send
+    timeout=timeout
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/connectionpool.py", line 639, in
+urlopen
+    _stacktrace=sys.exc_info()[2])
+  File "/usr/local/lib/python3.5/dist-packages/urllib3/util/retry.py", line 388, in incr
+ement
+    raise MaxRetryError(_pool, url, error or ResponseError(cause))
+urllib3.exceptions.MaxRetryError: HTTPSConnectionPool(host='query2.finance.yahoo.com', p
+ort=443): Max retries exceeded with url: /v11/finance/quoteSummary/CCO?modules=defaultKe
+yStatistics,assetProfile,financialData,calendarEvents,incomeStatementHistory,cashflowSta
+tementHistory,balanceSheetHistory (Caused by NewConnectionError('<urllib3.connection.Ver
+ifiedHTTPSConnection object at 0x7f3411777c50>: Failed to establish a new connection: [E
+rrno 101] Network is unreachable',))
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/usr/lib/python3.5/concurrent/futures/process.py", line 175, in _process_worker
+    r = call_item.fn(*call_item.args, **call_item.kwargs)
+  File "/home/nate/github/scrape_stocks/scrape_stockdata.py", line 569, in scrape_a_tick
+er_mongo
+    res = req.get(url, timeout=10)
+  File "/usr/local/lib/python3.5/dist-packages/requests/api.py", line 72, in get
+    return request('get', url, params=params, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/api.py", line 58, in request
+    return session.request(method=method, url=url, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/sessions.py", line 508, in reque
+st
+    resp = self.send(prep, **send_kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/sessions.py", line 618, in send
+    r = adapter.send(request, **kwargs)
+  File "/usr/local/lib/python3.5/dist-packages/requests/adapters.py", line 508, in send
+    raise ConnectionError(e, request=request)
+requests.exceptions.ConnectionError: HTTPSConnectionPool(host='query2.finance.yahoo.com$
+, port=443): Max retries exceeded with url: /v11/finance/quoteSummary/CCO?modules=defau$
+tKeyStatistics,assetProfile,financialData,calendarEvents,incomeStatementHistory,cashflo$
+StatementHistory,balanceSheetHistory (Caused by NewConnectionError('<urllib3.connection$
+VerifiedHTTPSConnection object at 0x7f3411777c50>: Failed to establish a new connection$
+ [Errno 101] Network is unreachable',))
+"""
+
+The above exception was the direct cause of the following exception:
+
+ConnectionError                           Traceback (most recent call last)
+/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py in run_code(sel$
+, code_obj, result)
+   2909                 #rprint('Running code', repr(code_obj)) # dbg
+-> 2910                 exec(code_obj, self.user_global_ns, self.user_ns)
+   2911             finally:
+
+<ipython-input-2-2c9937c17e38> in <module>()
+----> 1 daily_scrape_data()
+
+~/github/scrape_stocks/scrape_stockdata.py in daily_scrape_data()
+    637                     last_scrape = today_utc.date()
+--> 638                     print('scraping...')
+    639                     scrape_all_tickers_mongo_parallel()
+
+~/github/scrape_stocks/scrape_stockdata.py in scrape_all_tickers_mongo_parallel(tickers$
+    548     for t, r in jobs:
+--> 549         if r.result() is not None:
+    550             print('ticker:', t, 'result:', r.result())
+
+/usr/lib/python3.5/concurrent/futures/_base.py in result(self, timeout)
+    397             elif self._state == FINISHED:
+--> 398                 return self.__get_result()
+    399
+
+/usr/lib/python3.5/concurrent/futures/_base.py in __get_result(self)
+    356         if self._exception:
+--> 357             raise self._exception
+    358         else:
+
+ConnectionError: None: Max retries exceeded with url: /v11/finance/quoteSummary/CCO?mod$
+les=defaultKeyStatistics,assetProfile,financialData,calendarEvents,incomeStatementHisto$
+y,cashflowStatementHistory,balanceSheetHistory (Caused by None)
+
+During handling of the above exception, another exception occurred:
+
+AttributeError                            Traceback (most recent call last)
+/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py in showtracebac$
+(self, exc_tuple, filename, tb_offset, exception_only, running_compiled_code)
+   1827                         # in the engines. This should return a list of strings.
+-> 1828                         stb = value._render_traceback_()
+   1829                     except Exception:
+
+AttributeError: 'ConnectionError' object has no attribute '_render_traceback_'
+
+During handling of the above exception, another exception occurred:
+
+TypeError                                 Traceback (most recent call last)
+/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py in run_code(self
+, code_obj, result)
+   2925             if result is not None:
+   2926                 result.error_in_exec = sys.exc_info()[1]
+-> 2927             self.showtraceback(running_compiled_code=True)
+   2928         else:
+   2929             outflag = False
+
+/usr/local/lib/python3.5/dist-packages/IPython/core/interactiveshell.py in showtracebac$(self, exc_tuple, filename, tb_offset, exception_only, running_compiled_code)
+   1829                     except Exception:
+   1830                         stb = self.InteractiveTB.structured_traceback(etype,
+-> 1831                                             value, tb, tb_offset=tb_offset)
+   1832
+   1833                     self._showtraceback(etype, value, stb)
+
+/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py in structured_traceback($elf, etype, value, tb, tb_offset, number_of_lines_of_context)
+   1369         self.tb = tb
+   1370         return FormattedTB.structured_traceback(
+-> 1371             self, etype, value, tb, tb_offset, number_of_lines_of_context)
+   1372
+   1373
+
+/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py in structured_traceback($elf, etype, value, tb, tb_offset, number_of_lines_of_context)
+   1277             # Verbose modes need a full traceback
+   1278             return VerboseTB.structured_traceback(
+-> 1279                 self, etype, value, tb, tb_offset, number_of_lines_of_context
+   1280             )
+   1281         else:
+
+/usr/local/lib/python3.5/dist-packages/IPython/core/ultratb.py in structured_traceback($elf, etype, evalue, etb, tb_offset, number_of_lines_of_context)
+   1138             exception = self.get_parts_of_chained_exception(evalue)
+   1139             if exception:
+-> 1140                 formatted_exceptions += self.prepare_chained_exception_message($value.__cause__)
+   1141                 etype, evalue, etb = exception
+   1142             else:
+
+TypeError: Can't convert 'list' object to str implicitly
---
+++ stats_error.py
@@ -0,0 +1,48 @@
+ZYNE
+---------------------------------------------------------------------------
+_RemoteTraceback                          Traceback (most recent call last)
+_RemoteTraceback:
+"""
+Traceback (most recent call last):
+  File "/usr/lib/python3.5/concurrent/futures/process.py", line 175, in _process_worker
+    r = call_item.fn(*call_item.args, **call_item.kwargs)
+  File "/home/nate/github/scrape_stocks/scrape_stockdata.py", line 588, in scrape_a_ticker_mongo
+    for k in res.json()['quoteSummary']['result'][0].keys():
+TypeError: 'NoneType' object is not subscriptable
+"""
+
+The above exception was the direct cause of the following exception:
+
+TypeError                                 Traceback (most recent call last)
+<ipython-input-7-2c9937c17e38> in <module>()
+----> 1 daily_scrape_data()
+
+~/github/scrape_stocks/scrape_stockdata.py in daily_scrape_data()
+    636                 if today_utc.hour > open_days.loc[today_utc.date()]['market_close'].hour:
+    637                     last_scrape = today_utc.date()
+--> 638                     print('scraping...')
+    639                     scrape_all_tickers_mongo_parallel()
+    640                 else:
+
+~/github/scrape_stocks/scrape_stockdata.py in scrape_all_tickers_mongo_parallel(tickers)
+    547
+    548     for t, r in jobs:
+--> 549         if r.result() is not None:
+    550             print('ticker:', t, 'result:', r.result())
+    551     # old way of doing it, and wasn't working great
+
+/usr/lib/python3.5/concurrent/futures/_base.py in result(self, timeout)
+    396                 raise CancelledError()
+    397             elif self._state == FINISHED:
+--> 398                 return self.__get_result()
+    399
+    400             self._condition.wait(timeout)
+
+/usr/lib/python3.5/concurrent/futures/_base.py in __get_result(self)
+    355     def __get_result(self):
+    356         if self._exception:
+--> 357             raise self._exception
+    358         else:
+    359             return self._result
+
+TypeError: 'NoneType' object is not subscriptable
